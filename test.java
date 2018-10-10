39afa7104de243f8ca6005632cb6e70969f1f49d
Bchi <benjaminchi@web.de>
2018-10-10 13:20:16 +0200

    test

---
+++ test.java
@@ -0,0 +1,25 @@
+public class HelloWorld {
+	
+	private String msg = "test";
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+		
+		
+		System.out.println("1234");
+		
+        System.out.println("ffd");
+		System.out.println("ffd");
+		System.out.println("ffd");
+		
+		
+		System.out.println("ffd");
+
+        System.out.println("1234");
+		
+		System.out.println(msg);
+		
+		
+    }
+
+}
