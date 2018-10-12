144a714682919522a53d80f284df9bdc6be6b353
Bchi <benjaminchi@web.de>
2018-10-12 13:50:43 +0200

    test

--- test.java
+++ test.java
@@ -1,23 +1,10 @@
 public class HelloWorld {
 	
     public static void main(String[] args) {
-<<<<<<< HEAD
-        // Prints "Hello, World" to the terminal window.
-		
-		
-		System.out.println("xxx");
-		System.out.println("xxx");
-		System.out.println("xxx");
-		System.out.println("xxx");
-		System.out.println("xxx");
-		
-		System.out.println("xxx");
-=======
 		
 		Test test = new Test();
 		
 		test.test("Hello World");
->>>>>>> feature_x
     }
 	
 	public void test(String msg){
